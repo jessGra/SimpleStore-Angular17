82f2d7949fd158bda73bf0b7c2e51f951d8a7bbb
JesGra <jesus.egranada@gmail.com>
2024-05-13 19:14:56 -0500

    enable amazon fetch, show empty products message and update api keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   amazonDataApiUrl: 'https://real-time-amazon-data.p.rapidapi.com',
-  rapidAmazonAPIKey: 'e71a618504msh7d39cce66c98f3ep11c2bbjsndf8497466c5e',
+  // rapidAmazonAPIKey: 'e71a618504msh7d39cce66c98f3ep11c2bbjsndf8497466c5e',
+  rapidAmazonAPIKey: '8487859316msh09c507bd45f22a8p1b3210jsnd0ef9bef5b0e',
   rapidAmazonAPIHost: 'real-time-amazon-data.p.rapidapi.com',
 };
